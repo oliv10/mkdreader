5f6cf9d4c39b56482a2674e4ddda4fd1ff858f10
oliv10 <18117030+oliv10@users.noreply.github.com>
2026-03-16 20:58:07 +0000

    chore(formula): update source+bottles for v0.1.5

--- Formula/mkdreader.rb
+++ Formula/mkdreader.rb
@@ -1,15 +1,15 @@
 class Mkdreader < Formula
   desc "Standalone Markdown viewer with micro-like keybindings"
   homepage "https://github.com/oliv10/mkdreader"
-  url "https://github.com/oliv10/mkdreader/archive/refs/tags/v0.1.4.tar.gz"
-  sha256 "95cca8c182153390093a69cd618438963c859cb8832c448b1ceedd5c9ea7afd2"
+  url "https://github.com/oliv10/mkdreader/archive/refs/tags/v0.1.5.tar.gz"
+  sha256 "6d43f097515ec7f25c9d030314243b31212fae230cfc5c6539770004a70769c2"
   license "MIT"
 
   bottle do
-    root_url "https://github.com/oliv10/mkdreader/releases/download/v0.1.4"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "ba5f61baefe491de20cfa34c188f8a19abc3543c808e115f44e848ca150a4459"
-    sha256 cellar: :any_skip_relocation, arm64_linux:  "a1784a735030bdd7acf6f3007ffa250885fc6366f812d0d505fa554216536699"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "bab9b5eb76c5c9c9ea0ec1dd9792e9c8ca6502598bd89539b5946ba164454c60"
+    root_url "https://github.com/oliv10/mkdreader/releases/download/v0.1.5"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "31aded422f5e6967d1e20048d95eb99c083a7034a79c7c739533b3f3bb4bcf79"
+    sha256 cellar: :any_skip_relocation, arm64_linux:  "37a970229794916b109d470517a53613f6a9bae3134488d9e8f6b30b99feca10"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "e4d75d00adf8938c0b3bdf2f637abc56cb10f317d7a0c728324ad6240902f8ca"
   end
 
   depends_on "python@3.14"
